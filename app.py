22c18ee80a45704b45a37d373fbae484fb71e534
Hydroinfromatics <gurudev@iccwindia.org>
2024-07-12 16:51:01 +0530

    iot adding

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from map import map_bp
 from dashboard import dashboard_bp
 from contact import contact_bp
 from gallery import gallery_bp
+from iot import iot_bp
 app = Flask(__name__)
 
 app.register_blueprint(home_bp)
@@ -11,6 +12,7 @@ app.register_blueprint(map_bp)
 app.register_blueprint(dashboard_bp)
 app.register_blueprint(contact_bp)
 app.register_blueprint(gallery_bp)
+app.register_blueprint(iot_bp)
 
 port = 6060  # You can change this to any port number you want
 app.config['PORT'] = port
